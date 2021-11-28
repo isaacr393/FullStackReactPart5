548f79aad47fec86fd3030ab5ef86e6afe53d1c8
Isaac <isaacrivas140@gmail.com>
2021-11-28 01:03:57 -0400

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,26 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, {useState} from 'react'
+
+const Blog = ({blog}) => {
+  const [showDetail, setShowDetail] = useState(false)
+
+  const toggleDetails = () => setShowDetail(!showDetail)
+
+  const detailDisplayStyle = { display: showDetail?'':'none' }
+  return(
+    <div style={{border:'1px solid black', margin:'10px'}}>
+      {blog.title}  &nbsp;
+      <button onClick={toggleDetails} >
+        {showDetail?'Hide':'Details'}
+      </button>
+      
+      <div style={detailDisplayStyle}>
+        {blog.author} <br />
+        {blog.likes} <button>like</button>
+      </div>      
+    </div>
+  ) 
+}
+
+
 
 export default Blog
