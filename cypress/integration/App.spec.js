6bd49b6c52daeae52c49c2e542f8766d370596d4
Isaac <isaacrivas140@gmail.com>
2021-12-01 23:36:39 -0400

    5.18

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -2,6 +2,13 @@ describe( 'Blog App', () => {
 
     beforeEach( () => {
         cy.request('POST','http://localhost:3001/api/testing/clear')
+        
+        let user = {
+            "user": "Isaac",
+            "username": "Isaac",
+            "password": "123",
+        }
+        cy.request('POST','http://localhost:3001/api/users', user)
         cy.visit('http://localhost:3000')
     })
 
@@ -9,5 +16,24 @@ describe( 'Blog App', () => {
         cy.get('#usernameInput')
         cy.get('#passwordInput')
         cy.contains('Log in')
+    })
+
+    describe('Login', () => {
+        
+        it('Successfull with rigth credentials', () => {
+            cy.get('#usernameInput').type('Isaac')
+            cy.get('#passwordInput').type('123')
+            cy.contains('Log in').click()
+
+            cy.contains('Isaac logged in')
+        })
+
+        it.only('Fails with wrongs credentials', () => {
+            cy.get('#usernameInput').type('Isaac')
+            cy.get('#passwordInput').type('wrong')
+            cy.contains('Log in').click()
+
+            cy.contains('Invalid Credentials')
+        })
     })
 })
